ecd81321c15e63655628fb8045b2c4cb649650f9
fathimasilviyak <silviyakvm@gmail.com>
2023-04-03 13:34:59 +0530

    conditional list comprehension

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ print(new_name)
 
 range_list = [2 * number for number in range(1, 5)]
 print(range_list)
+
+names = ["Anu", "Anamika", "Anju", "Manu", "Haritha", "Amayana", "rani"]
+short_names = [name for name in names if len(name) < 5]
+print(short_names)
+long_names = [name.upper() for name in names if len(name) >=5]
+print(long_names)
