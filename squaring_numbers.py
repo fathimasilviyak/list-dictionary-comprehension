28765127182b314c5dc172c9da361bf6904caf2c
fathimasilviyak <silviyakvm@gmail.com>
2023-04-03 13:48:50 +0530

    filter even numbers

--- squaring_numbers.py
+++ squaring_numbers.py
@@ -2,3 +2,6 @@ numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 # squared_numbers = [number * number for number in numbers]
 squared_numbers = [number**2 for number in numbers]
 print(squared_numbers)
+
+even_numbers =  [number for number in numbers if number%2 ==0]
+print(even_numbers)
