14a2f0e73eef2a1e217823a740c3760817a53afc
fathimasilviyak <silviyakvm@gmail.com>
2023-04-03 15:23:13 +0530

    dictionary comprehension from dictionary

--- dictionary_comprehension.py
+++ dictionary_comprehension.py
@@ -2,3 +2,6 @@ import random
 names = ["Anu", "Anamika", "Anju", "Manu", "Haritha", "Amayana", "rani"]
 student_scores = {name: random.randint(1, 100) for name in names}
 print(student_scores)
+
+passed_students = {student: score for (student, score) in student_scores.items() if score >= 60}
+print(passed_students)
