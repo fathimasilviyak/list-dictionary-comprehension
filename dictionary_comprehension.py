c804c7b058836b4003c0e91828c9f57a55310f2f
fathimasilviyak <silviyakvm@gmail.com>
2023-04-03 15:38:32 +0530

    dictionary comprehension from a string

--- dictionary_comprehension.py
+++ dictionary_comprehension.py
@@ -5,3 +5,7 @@ print(student_scores)
 
 passed_students = {student: score for (student, score) in student_scores.items() if score >= 60}
 print(passed_students)
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+words_dictionary = {word: len(word) for word in sentence.split()}
+print(words_dictionary)
